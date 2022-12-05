0346366afd8ef800855049d1829cf9eccab0442f
Eric Carlson <eric.carlson2972@gmail.com>
2022-12-04 21:19:24 -0500

    add basic branding and styles to the header and the

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -20,54 +20,54 @@ const Header = () => {
     <header>
       {/* <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect> */}
       <Navbar expand="lg" collapseOnSelect>
-        <Container>
-          <LinkContainer to="/">
-            {/* <Navbar.Brand>Tuber</Navbar.Brand> */}
-            <Navbar className="navbar-brand-custom">
-              Tuber<i class="fas fa-seedling"></i>
-            </Navbar>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Route render={({ history }) => <SearchBox history={history} />} />
-            <Nav className="ml-auto">
-              <LinkContainer to="/cart" className="btn cart-button">
+        {/* <Container> */}
+        <LinkContainer to="/">
+          {/* <Navbar.Brand>Tuber</Navbar.Brand> */}
+          <Navbar className="navbar-brand-custom">
+            Tuber<i class="fas fa-seedling"></i>
+          </Navbar>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Route render={({ history }) => <SearchBox history={history} />} />
+          <Nav className="ml-auto">
+            <LinkContainer to="/cart" className="btn cart-button">
+              <Nav.Link>
+                <i className="fas fa-shopping-cart"></i> Cart
+              </Nav.Link>
+            </LinkContainer>
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id="username">
+                <LinkContainer to="/profile">
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <LinkContainer to="/login" className="btn signup-button">
                 <Nav.Link>
-                  <i className="fas fa-shopping-cart"></i> Cart
+                  <i className="fas fa-user"></i> Sign In
                 </Nav.Link>
               </LinkContainer>
-              {userInfo ? (
-                <NavDropdown title={userInfo.name} id="username">
-                  <LinkContainer to="/profile">
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
-                  </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    Logout
-                  </NavDropdown.Item>
-                </NavDropdown>
-              ) : (
-                <LinkContainer to="/login" className="btn signup-button">
-                  <Nav.Link>
-                    <i className="fas fa-user"></i> Sign In
-                  </Nav.Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <NavDropdown title="Admin" id="adminmenu">
+                <LinkContainer to="/admin/userlist">
+                  <NavDropdown.Item>Users</NavDropdown.Item>
                 </LinkContainer>
-              )}
-              {userInfo && userInfo.isAdmin && (
-                <NavDropdown title="Admin" id="adminmenu">
-                  <LinkContainer to="/admin/userlist">
-                    <NavDropdown.Item>Users</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/admin/productlist">
-                    <NavDropdown.Item>Products</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/admin/orderlist">
-                    <NavDropdown.Item>Orders</NavDropdown.Item>
-                  </LinkContainer>
-                </NavDropdown>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+                <LinkContainer to="/admin/productlist">
+                  <NavDropdown.Item>Products</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/admin/orderlist">
+                  <NavDropdown.Item>Orders</NavDropdown.Item>
+                </LinkContainer>
+              </NavDropdown>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+        {/* </Container> */}
       </Navbar>
     </header>
   );
