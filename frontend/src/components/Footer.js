7daedaadec943543562550688fee5d86a7c3bd06
Eric Carlson <eric.carlson2972@gmail.com>
2023-01-01 22:00:35 -0500

    Add footer content and layout

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,14 +1,50 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
     <footer>
-      <Container>
-        <Row>
-          {/* <Col className='text-center py-3'>Copyright &copy; ProShop</Col> */}
-        </Row>
-      </Container>
+      <div class="footer-container">
+        <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+          <div class="col mb-3">
+            <a class="navbar-brand-custom" href="#">
+              Tuber <i class="fas fa-seedling"></i>
+            </a>
+            <div class="social-media">
+              <img class="app-stores" src="/images/get-it-on-app-store.svg" />
+              <img class="app-stores" src="/images/get-it-on-google-play.png" />
+            </div>
+          </div>
+          <div class="col mb-3"></div>
+          <div class="col mb-3">
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2">Help</li>
+              <li class="nav-item mb-2">Learn to garden forums</li>
+              <li class="nav-item mb-2">Add your garden</li>
+              <li class="nav-item mb-2">FAQs</li>
+              <li class="nav-item mb-2">About Tuber</li>
+            </ul>
+          </div>
+          <div class="col mb-3">
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2">Promotions</li>
+              <li class="nav-item mb-2">Features</li>
+              <li class="nav-item mb-2">Pricing</li>
+              <li class="nav-item mb-2">Lorem</li>
+              <li class="nav-item mb-2">Ipsum</li>
+            </ul>
+          </div>
+          <div class="col mb-3">
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2">Lorem</li>
+              <li class="nav-item mb-2">Ipsum</li>
+              <li class="nav-item mb-2">Dolor</li>
+              <li class="nav-item mb-2">Sit</li>
+              <li class="nav-item mb-2">Amet</li>
+            </ul>
+          </div>
+        </footer>
+      </div>
     </footer>
   );
 };
--- frontend/src/screens/LandingScreen.js
+++ frontend/src/screens/LandingScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const LandingScreen = () => {
   return (
-    <div className="container">
+    <div className="landing-container">
       <div className="hero-image">
         <div className="hero-text">It's Uber Eats for gardens</div>;
       </div>
